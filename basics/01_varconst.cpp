d738cde3d1f95aaa86819c4724abfa7ab59e101e
Samay Masram <samaymasram1404@gmail.com>
2026-03-25 18:34:21 +0530

    old code

--- basics/01_varconst.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int score;
-    score = 100;
-    int balance = 500;
-
-    return 0;
-}
--- basics/hello.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using std::cout;
-using std::endl;
- 
-int main(){
-    cout<<"Hello Cpp"<<endl;
-    return 0;
-}
