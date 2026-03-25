ecc09c46430c1d2d5663316b53abc6db80725eaf
Samay Masram <samaymasram1404@gmail.com>
2026-03-25 19:42:56 +0530

    datatypes

---
+++ 02_primitives/data.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int teaLeaves = 50;
+    float waterTemp = 85.5;
+    double priceOfTea = 299.99;
+    char teagrade = 'A';
+    bool isTeaReady = false; 
+    cout<<isTeaReady<<endl; 
+    return 0;
+}
