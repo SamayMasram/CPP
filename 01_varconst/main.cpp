fbb106981d5e1fc2d0378936ea18985fe1b5d48c
Samay Masram <samaymasram1404@gmail.com>
2026-03-25 19:09:55 +0530

    variables & constants

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //cout<<"Welcome student 1"<<endl;
+    /*
+    cout<<"Welcome student 2"<<endl;
+    */
+   cout<<"Welcome student 3"<<endl;
+   int score;
+   score = 100;
+   const int max_score = 100;
+   //max_score = 90; // This will cause an error because max_score is a constant
+   return 0;
+}
