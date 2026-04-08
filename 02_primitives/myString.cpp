54333e51c0810053d7f7fe68e058ecf21da6c60f
Samay Masram <samaymasram1404@gmail.com>
2026-04-08 22:01:09 +0530

    String

---
+++ 02_primitives/myString.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string favoriteTea = "masala tea";
+    string description = "known as \"normal\" tea"; //escape character
+
+    cout<<favoriteTea<<endl;
+    cout<<description<<endl;
+    return 0;
+}
