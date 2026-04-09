a7bda9310f294cec85d408acbd07517b84ef3063
Samay Masram <samaymasram1404@gmail.com>
2026-04-09 20:37:26 +0530

    task 3 (IF-ELSE-IF)

---
+++ 04_conditionals/task3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int noOfCups;
+    cout<<"How many cups of tea do you purchased: "<<endl;    
+    cin>>noOfCups;
+    if(noOfCups > 20){
+        cout<<"You get 20% discount"<<endl;
+    }else if(10 < noOfCups && noOfCups <= 20){
+        cout<<"You get 10% discount"<<endl;
+    }else{
+        cout<<"No discount available"<<endl;
+    }
+    return 0;
+}
