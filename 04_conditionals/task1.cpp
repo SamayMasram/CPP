e8598b6555f10ca9fad64b913d9f6384a485dc6d
Samay Masram <samaymasram1404@gmail.com>
2026-04-09 20:36:27 +0530

    task 1 (IF)

---
+++ 04_conditionals/task1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string uChoice;
+    cout<<"Which tea do you want: "<<endl;
+    cin>>uChoice;
+    if(uChoice == "Green_tea"){
+        cout<<"Order Confirmed"<<endl;
+    }
+    return 0;
+}
+
+// only IF condition
