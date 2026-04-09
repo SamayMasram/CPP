7925953d235a39aa3fa88b5898a96016d5ebd76b
Samay Masram <samaymasram1404@gmail.com>
2026-04-09 20:32:51 +0530

    Assignment operators

---
+++ 03_operators/assign.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int teaBags;
+    cout<<"Enter the number of tea bags you have: "<<endl;
+    cin>>teaBags;
+
+    if(teaBags > 10){
+        teaBags += 5;
+    }
+    cout<<"Your total bags are "<<teaBags<<endl;
+    return 0;
+}
